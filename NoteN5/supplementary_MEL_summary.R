b16a8daa9df170f401564bae5a723b3c98b67e9d
fuerzhou <yuzhou0610@gmail.com>
2022-12-20 13:58:30 +1100

    Added Note N5 plots.

---
+++ NoteN5/supplementary_MEL_summary.R
@@ -0,0 +1,16 @@
+load("../Figure6/Objects/Pan_immune_spe.RData")
+library(SPIAT)
+
+plot_cell_categories(primary, c("Undefined","MEL", "T", "B", "DC"),
+                     c("lightgray", "red", "blue", "darkgreen", "orange"),
+                     feature_colname = "Cell.Type", cex = 0.4, layered = TRUE)
+
+plot_cell_categories(MET, c("Undefined","MEL", "T", "B", "DC"),
+                     c("lightgray", "red", "blue", "darkgreen", "orange"),
+                     feature_colname = "Cell.Type", cex = 0.2, layered = TRUE)
+
+plot_cell_categories(REL, c("Undefined","MEL", "T", "B", "DC"),
+                     c("lightgray", "red", "blue", "darkgreen", "orange"),
+                     feature_colname = "Cell.Type", cex = 0.2, layered = TRUE)
+
+# Code for cell porportions see "../Figure6/Fig6-Melanoma_margin_detection.Rmd"
